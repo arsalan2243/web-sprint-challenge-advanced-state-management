68af1ae41dd6f94cff727ee0aab4cf3262e6f48e
arsalan rahimi <years1373@yahoo.com>
2021-12-10 10:56:14 -0800

    finished

--- src/App.js
+++ src/App.js
@@ -3,26 +3,33 @@ import React, { Component } from "react"
 import AddForm from "./components/AddForm"
 import SmurfList from "./components/SmurfList"
 import Header from "./components/Header"
+
 import { connect } from "react-redux"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./App.css"
 import { fetchSmurfs } from "./actions/index"
 
-const App = () => {
-  return (
-    <div className="App">
-      <Header />
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchSmurfs()
+  }
 
-      <main>
-        <SmurfList />
-        <AddForm />
-      </main>
-    </div>
-  )
+  render() {
+    return (
+      <div className="App">
+        <Header />
+
+        <main>
+          <SmurfList />
+          <AddForm />
+        </main>
+      </div>
+    )
+  }
 }
 
 export default connect(null, { fetchSmurfs })(App)
 
 //Task List:
 //1. Connect the fetchSmurfs actions to the App component.
-//2. Call the fetchSmurfs action when the component mounts.
+//2. Call the fetchSmurfs action when the component firs
