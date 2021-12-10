6d23e05a3e213571e6d7840740b6ddf26be47e83
arsalan rahimi <years1373@yahoo.com>
2021-12-10 10:14:56 -0800

    action and reducers finished

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,31 +1,11 @@
-import React from "react"
-import ReactDOM from "react-dom"
-
-import "./index.css"
-import App from "./App"
-
-import { createStore, applyMiddleware } from "redux"
-import reducer from "./reducers"
-
-import thunk from "redux-thunk"
-import logger from "redux-logger"
-import { Provider } from "react-redux"
-
-const store = createStore(reducer, applyMiddleware(thunk, logger))
-
-const { worker } = require("./mocks/browser")
-worker.start()
-
-const rootElement = document.getElementById("root")
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  rootElement
-)
+import axios from "axios"
+export const FATCH_START = "FATCH_START"
+export const FATCH_SUCESS = "FATCH_SUCESS"
+export const FATCH_ERROR = "FATCH_ERROR"
+export const ADD_NEW_SMURF = "ADD_NEW_SMURF"
+export const ADD_NEW_ERROR = "ADD_NEW_ERROR"
 
 //Task List:
-//1. Add in all necessary components and libary methods.
-//2. Create a store that includes thunk and logger middleware support.
-//3. Wrap the App component in a react-redux Provider element.
+//1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
+//2. Add a standard action that allows us to add new smurf (including the name, nickname, position, summary)
+//3. Add a standard action that allows us to set the value of the error message slice of state.
